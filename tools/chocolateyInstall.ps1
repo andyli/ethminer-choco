ae57ba08e887878bb6fad51aa9e6b57a87e99e0a
Andy Li <andy@onthewings.net>
2019-06-17 18:16:19 +0800

    update to 0.17.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
 Install-ChocolateyZipPackage -PackageName 'ethminer' `
- -Url64bit 'https://github.com/ethereum-mining/ethminer/releases/download/v0.14.0/ethminer-0.14.0-Windows.zip' `
- -Checksum64 'bc0fadbaa98d113663b588e2cf9dc61e72745363645f1f46dcddc1526be88a98' -ChecksumType64 'sha256' `
+ -Url64bit 'https://github.com/ethereum-mining/ethminer/releases/download/v0.17.1/ethminer-0.17.1-cuda9.0-windows-amd64.zip' `
+ -Checksum64 '86414b48c7def79c36e1c4474bcd3b13f06ceb33ad084ac4d69199d320bcb581' -ChecksumType64 'sha256' `
  -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
